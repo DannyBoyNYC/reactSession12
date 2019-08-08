82e84e8ede1d8098ec66927a4659a31f8acfa0b6
DannyBoyNYC <daniel.deverell@gmail.com>
2019-08-08 18:42:54 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const fileUpload = require('express-fileupload');
+
+const recipeModel = require('./api/recipe.model');
+const recipeControllers = require('./api/recipe.controllers');
+
+require('dotenv').config();
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  next();
+});
+app.use(fileUpload());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ extended: false }));
+app.use(express.static('public'));
+
+// const dataBaseURL =
+//   'mongodb+srv://daniel:dd2345@recipes-3k4ea.mongodb.net/test?retryWrites=true&w=majority';
+
+const dataBaseURL = process.env.DATABASE;
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/api/recipes', recipeControllers.findAll);
+app.get('/api/recipes/:id', recipeControllers.findById);
+app.post('/api/recipes', recipeControllers.add);
+app.put('/api/recipes/:id', recipeControllers.update);
+app.delete('/api/recipes/:id', recipeControllers.delete);
+app.get('/api/import', recipeControllers.import);
+app.get('/api/killall', recipeControllers.killall);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(dataBaseURL, { useNewUrlParser: true })
+  .then(() => console.log('MongoDb connected'))
+  .catch(err => console.warn(err));
+
+app.listen(PORT, () => console.log(`Server running at port ${PORT}`));
